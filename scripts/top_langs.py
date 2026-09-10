02870091a058b0e796e77abbebb1f923f690faa8
AlP <203755544+AIPEAC@users.noreply.github.com>
2026-09-10 16:41:16 +0800

    chore: update links for scripts

--- scripts/top_langs.py
+++ scripts/top_langs.py
@@ -18,7 +18,7 @@ import sys
 import urllib.request
 from collections import Counter
 
-OWNERS = ["AIPEAC", "AIPEACM", "AIPEACMS", "AIPEACS"]
+OWNERS = ["AIPEAC", "AIPEACM", "AIPEACMS", "AIPEACS","AIPBS"]
 LANG_COUNT = 8
 BAR_WIDTH = 300
 BAR_HEIGHT = 10
